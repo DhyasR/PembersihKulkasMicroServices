c8ac3f4cbee6f4abc24c73915a15a3897d163454
DhyasR <119585072+DhyasR@users.noreply.github.com>
2025-05-09 15:02:41 +0700

    fix docker-compose file

---
+++ down-all.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+# Daftar path ke docker-compose.yml
+COMPOSE_PATHS=(
+  "./api-gateway"
+  "./chatpage"
+  "./homepage"
+  "./kulkaskupage"
+  "./profilepage"
+)
+
+for path in "${COMPOSE_PATHS[@]}"; do
+  echo "🛑 Stopping services in $path"
+  docker-compose -f "$path/docker-compose.yml" down
+done
+
+echo "🧹 All services stopped and cleaned up!"
---
+++ run_all.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+# Daftar path ke docker-compose.yml
+COMPOSE_PATHS=(
+  "./api-gateway"
+  "./chatpage"
+  "./homepage"
+  "./kulkaskupage"
+  "./profilepage"
+)
+
+for path in "${COMPOSE_PATHS[@]}"; do
+  echo "🚀 Starting services in $path"
+  docker-compose -f "$path/docker-compose.yml" up -d --build
+done
+
+echo "✅ All services started successfully!"
